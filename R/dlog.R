cf579928813f1044d00c374bd96a7edf25a95b48
SangseokBae <ssb9018@ajou.ac.kr>
2023-04-06 09:10:22 +0900

    first commit

--- R/dlog.R
+++ R/dlog.R
@@ -4,6 +4,26 @@
 #' 
 
 dlog<-function(x){
+  tmp_k<-0
+  
+  for(i in 1:length(x)){
+  if(is.na(x[i]) & !is.na(x[i+1])  ) {
+  tmp_k <- (i+1)
+  break
+  }}
+  
+  
+  if(tmp_k>0) {
+  tmp_x<-x[tmp_k:length(x)]
+  logx<-log(tmp_x)
+  m2<- logx[-1]-logx[-length(tmp_x)]
+  return( c(NA[1:tmp_k], m2) )
+  }
+
+    
+  if(tmp_k==0) {
   logx<-log(x)
-  c(NA, (logx[-1]-logx[-length(x)]))
+  return( c(NA, (logx[-1]-logx[-length(x)])) )
+  }
+
 }
