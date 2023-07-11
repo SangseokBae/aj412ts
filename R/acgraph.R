b46076cc9a7eda2140b32c9a8a14718639b754cd
SangseokBae <ssb9018@ajou.ac.kr>
2023-07-11 18:46:49 +0900

    first commit

--- R/acgraph.R
+++ R/acgraph.R
@@ -7,16 +7,13 @@ acgraph=function(x, lags=15){
   r=c(); v=c()
   n=length(x)
   for (k in 1:lags){
-    r=c(r, ac(x,k)) 
-    v=c(v,(1+2*sum(r[-1]^2))/n ) }
-barplot(r, lwd=2, col='red', ylim=c(-1, 1), border=NA,
-       xlab='LAG', ylab="ACF", names.arg=1:lags); 
-   abline(h=0)
-#  points(1:max(lags),2*sqrt(v), lty=3, type='l', col='blue');
-#  points(1:max(lags),-2*sqrt(v), lty=3, type='l', col='blue');
-#  names(r)=1:lags
-   abline(h=2/sqrt(n), lty=3, col='blue')
-   abline(h=-2/sqrt(n), lty=3, col='blue')
-   print(r)
+     r=c(r, ac(x,k)) }
+  
+  barplot(r, col='steelblue', ylim=c(-1, 1), border=NA, xlab='LAG', ylab="ACF", names.arg=1:lags); 
+  box()
+  abline(h=0)
+  abline(h=2/sqrt(n), lty=3, col='black')
+  abline(h=-2/sqrt(n), lty=3, col='black')
+  print(r)
   return(r) }
   
--- R/pacgraph.R
+++ R/pacgraph.R
@@ -8,10 +8,10 @@ pacgraph=function(x, lags=15){
  b=c() 
  for (k in 1:lags){
   b=c(b, pac(x,k))}
-  barplot(b, ylab='PACF', xlab='LAG', ylim=c(-1,1), border=NA,
-           col='#f23500ff', names.arg=1:lags)
+  barplot(b, ylab='PACF', xlab='LAG', ylim=c(-1,1), border=NA, col='steelblue', names.arg=1:lags)
   box()
   abline(h=0)
-  abline(h=2/sqrt(n), lty=3, col='blue')
-  abline(h=-2/sqrt(n), lty=3, col='blue')
+  abline(h=2/sqrt(n), lty=3, col='black')
+  abline(h=-2/sqrt(n), lty=3, col='black')
+  print(b)
   return(b) }
